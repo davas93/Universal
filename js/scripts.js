37ba3d8fa126e0aa5172198134b546f2a731ccb3
davas93 <vasin.da93@gmail.com>
2020-08-10 15:21:43 +0300

    Разметка и стилизация header

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Отложенная загрузка картинок
+window.onload = () => {
+  var imgDefer = document.getElementsByTagName("img");
+  for (var i = 0; i < imgDefer.length; i++) {
+    if (imgDefer[i].getAttribute("data-src")) {
+      imgDefer[i].setAttribute("src", imgDefer[i].getAttribute("data-src"));
+    }
+  }
+};
