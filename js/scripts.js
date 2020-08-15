d4b835db76cc16c60050ab8b8a1847d32ad46ce5
davas93 <vasin.da93@gmail.com>
2020-08-15 08:57:43 +0300

    Валидация формы подписки

--- js/scripts.js
+++ js/scripts.js
@@ -114,3 +114,20 @@ $(".comments__button-more").on("click", function () {
       ? (btn.innerHTML = "Hide")
       : (btn.innerHTML = "Load more");
 });
+
+//Валидация форм
+$(".form").each(function () {
+  $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "Name must be at least 2 letters long",
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com",
+      },
+    },
+  });
+});
